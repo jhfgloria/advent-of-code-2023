40a4fa28579b3b54acd030189ba74d33fbc7698c
Joao Gloria <jhfgloria@gmail.com>
2023-12-07 19:32:50 +0000

    day 3

--- day_three/day_three.go
+++ day_three/day_three.go
@@ -3,41 +3,76 @@ package day_three
 import (
 	"fmt"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
-type Line struct {
-	Index       uint16
-	Symbols     []Symbol
-	PartNumbers []PartNumber
-}
-
-type Symbol struct {
-	Position uint8
-}
-
-type PartNumber struct {
-	Number uint16
-	Start  uint8
-	End    uint8
-}
-
 var symbolExpression = regexp.MustCompile(`[^\w\.]`)
+var partNumberExpression = regexp.MustCompile(`\d`)
 
-func StarOne(input string) uint16 {
-	lines := strings.Split(input, "\n")
-	var allLines []Line
+func countAsPartNumber(lines []string, line []rune, number int, previousLineIndex int, nextLineIndex int, previousCharacterIndex int, nextCharacterIndex int, start int, end int) uint16 {
+	if previousCharacterIndex >= 0 {
+		if symbolExpression.MatchString(string(line[previousCharacterIndex])) {
+			return uint16(number)
+		}
+	}
 
-	for _, l := range lines {
-		chars := []rune(l)
-		line := Line{}
-		for i := 0; i < len(chars); i++ {
-			if symbolExpression.MatchString(string(chars[i])) {
-				line.Index = uint16(i)
-				fmt.Println("SYMBOL")
+	if nextCharacterIndex < len(line) {
+		if symbolExpression.MatchString(string(line[nextCharacterIndex])) {
+			return uint16(number)
+		}
+	}
+
+	if previousLineIndex > 0 {
+		for i := max(0, start-1); i < min(end+1, len(line)-1); i++ {
+			if symbolExpression.MatchString(string(lines[previousLineIndex][i])) {
+				return uint16(number)
 			}
 		}
 	}
 
-	return uint16(0)
+	if nextLineIndex < len(lines)-1 {
+		for i := max(0, start-1); i < min(end+1, len(line)-1); i++ {
+			if symbolExpression.MatchString(string(lines[nextLineIndex][i])) {
+				return uint16(number)
+			}
+		}
+	}
+
+	return 0
+}
+
+func StarOne(input string) uint16 {
+	lines := strings.Split(input, "\n")
+	result := uint16(0)
+
+	for lineIndex, line := range lines {
+		fmt.Println(line)
+
+		strNumber := ""
+		start := -1
+
+		for charIndex, character := range line {
+			if partNumberExpression.MatchString(string(character)) {
+				strNumber += string(character)
+				if start == -1 {
+					start = charIndex
+				}
+			}
+			if strNumber != "" && (!partNumberExpression.MatchString(string(character)) || charIndex == len(line)-1) {
+				number, _ := strconv.Atoi(strNumber)
+				previousCharacterIndex := start - 1
+				nextCharacterIndex := charIndex
+				previousLineIndex := lineIndex - 1
+				nextLineIndex := lineIndex + 1
+
+				result += countAsPartNumber(lines, []rune(line), number, previousLineIndex, nextLineIndex, previousCharacterIndex, nextCharacterIndex, start, charIndex)
+
+				strNumber = ""
+				start = -1
+			}
+		}
+	}
+
+	return result
 }
--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"aoc/2023/day_two"
+	"aoc/2023/day_three"
 	"aoc/2023/requests"
 	"fmt"
 )
 
 func main() {
-	input := requests.GetExercise("53616c7465645f5ff687033561bd0e29fa45f5decbefb866b175133376a0981e9c76ee345e33947135828ccc716dff1d09ad3e53f648bbc5e31b358790966b65", 2)
-	result := day_two.StarTwo(input)
+	input := requests.GetExercise("53616c7465645f5ff687033561bd0e29fa45f5decbefb866b175133376a0981e9c76ee345e33947135828ccc716dff1d09ad3e53f648bbc5e31b358790966b65", 3)
+	result := day_three.StarOne(input)
 	fmt.Println(result)
 }
